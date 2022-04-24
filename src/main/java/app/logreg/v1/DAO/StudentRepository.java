5e32f5a101b3f70e620e26762b529f3b61175471
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-24 19:47:18 +0300

    Login/Register

--- src/main/java/app/logreg/v1/DAO/StudentRepository.java
+++ src/main/java/app/logreg/v1/DAO/StudentRepository.java
@@ -1,10 +1,12 @@
 package app.logreg.v1.DAO;
 
 import app.logreg.v1.AppUser.AppUser;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Repository
 @Transactional(readOnly = true)
 public interface StudentRepository {
     Optional<AppUser> findByEmail(String email);
--- src/main/java/app/logreg/v1/Service/AppUserService.java
+++ src/main/java/app/logreg/v1/Service/AppUserService.java
@@ -3,7 +3,9 @@ package app.logreg.v1.Service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AppUserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
