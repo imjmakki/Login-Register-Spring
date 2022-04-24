ab2c7f1078fa4dc5ce954ffd9b2e6a007c8a3cea
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-24 19:46:11 +0300

    Login/Register

--- src/main/java/app/logreg/v1/DAO/StudentRepository.java
+++ src/main/java/app/logreg/v1/DAO/StudentRepository.java
@@ -1,7 +1,11 @@
 package app.logreg.v1.DAO;
 
+import app.logreg.v1.AppUser.AppUser;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Transactional(readOnly = true)
 public interface StudentRepository {
+    Optional<AppUser> findByEmail(String email);
 }
