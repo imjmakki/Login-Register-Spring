1ed888cbe34a46c29ac126747f5b39436cd9b56b
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-24 20:01:29 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Registration/RegistrationController.java
+++ src/main/java/app/logreg/v1/Registration/RegistrationController.java
@@ -1,7 +1,9 @@
 package app.logreg.v1.Registration;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "api/v1/register")
 public class RegistrationController {
 }
