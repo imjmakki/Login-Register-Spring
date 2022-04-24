cc671ebcb4d8293f622bfdbda96014514a45c50b
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-24 19:14:44 +0300

    Login/Register

--- src/main/java/app/logreg/v1/AppUser/AppUser.java
+++ src/main/java/app/logreg/v1/AppUser/AppUser.java
@@ -6,6 +6,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 
 public class AppUser implements UserDetails {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
