f57fba9e6066bd845f8a86b77128fa8012af896c
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-24 19:58:23 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Service/AppUserService.java
+++ src/main/java/app/logreg/v1/Service/AppUserService.java
@@ -18,7 +18,7 @@ public class AppUserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
         return appUserRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException(String.format(
-                        USER_NOT_FOUND_MSG, email)));
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format(USER_NOT_FOUND_MSG, email)));
     }
 }
