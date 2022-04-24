d0940ef2e344d3139cf3616e158b91e642ad0eac
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-24 19:56:23 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Service/AppUserService.java
+++ src/main/java/app/logreg/v1/Service/AppUserService.java
@@ -11,13 +11,13 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class AppUserService implements UserDetailsService {
 
-    private final static String USER_NOT_FOUND = "user with email %s not found";
+    private final static String USER_NOT_FOUND_MSG = "user with email %s not found";
     private final AppUserRepository appUserRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
         return appUserRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND, email)));
+                .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
     }
 }
