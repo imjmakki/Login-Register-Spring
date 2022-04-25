9f21e355b2d34a1038928912241790f6ce4c063a
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-25 12:51:32 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Security/Config/EmailValidator.java
+++ src/main/java/app/logreg/v1/Security/Config/EmailValidator.java
@@ -7,6 +7,6 @@ import java.util.function.Predicate;
 public class EmailValidator implements Predicate<String> {
     @Override
     public boolean test(String s) {
-        return false;
+        return true;
     }
 }
