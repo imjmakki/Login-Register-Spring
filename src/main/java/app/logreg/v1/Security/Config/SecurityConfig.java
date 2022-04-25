913b96e8fe358884b0af2b089a162b85afd88c61
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-25 11:29:45 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Security/Config/SecurityConfig.java
+++ src/main/java/app/logreg/v1/Security/Config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package app.logreg.v1.Security.Config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,5 +24,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider()
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+
     }
 }
