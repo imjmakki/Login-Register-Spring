2749c32cbd5024cfb751da675fd79350d232d103
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-25 19:06:04 +0300

    Login/Register

--- src/main/java/app/logreg/v1/DAO/ConfirmationTokenRepository.java
+++ src/main/java/app/logreg/v1/DAO/ConfirmationTokenRepository.java
@@ -1,4 +1,9 @@
 package app.logreg.v1.DAO;
 
-public interface ConfirmationTokenRepository {
+import app.logreg.v1.Security.Config.ConfirmationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
 }
