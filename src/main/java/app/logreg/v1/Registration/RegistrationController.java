8782a799b70d1e748f45341ea731027283df59dc
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-25 11:49:04 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Registration/RegistrationController.java
+++ src/main/java/app/logreg/v1/Registration/RegistrationController.java
@@ -3,6 +3,7 @@ package app.logreg.v1.Registration;
 import app.logreg.v1.DAO.RegistrationRequest;
 import app.logreg.v1.Service.RegistrationService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/app/logreg/v1/Service/RegistrationService.java
+++ src/main/java/app/logreg/v1/Service/RegistrationService.java
@@ -1,7 +1,9 @@
 package app.logreg.v1.Service;
 
 import app.logreg.v1.DAO.RegistrationRequest;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
         return null;
