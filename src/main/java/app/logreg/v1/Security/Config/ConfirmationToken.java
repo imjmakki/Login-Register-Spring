1a68616a234969d2a7c03c7aba09da4f2dfb9e49
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-25 14:06:50 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Security/Config/ConfirmationToken.java
+++ src/main/java/app/logreg/v1/Security/Config/ConfirmationToken.java
@@ -1,6 +1,10 @@
 package app.logreg.v1.Security.Config;
 
+import java.time.LocalDateTime;
+
 public class ConfirmationToken {
     private Long id;
     private String token;
+    private LocalDateTime localDateTime;
+    private LocalDateTime expiredAt;
 }
