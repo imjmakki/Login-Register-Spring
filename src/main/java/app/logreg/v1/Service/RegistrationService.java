db0d0f32430b387f9f29a3105200c0e8089f7225
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-25 12:57:06 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Service/RegistrationService.java
+++ src/main/java/app/logreg/v1/Service/RegistrationService.java
@@ -1,11 +1,16 @@
 package app.logreg.v1.Service;
 
 import app.logreg.v1.DAO.RegistrationRequest;
+import app.logreg.v1.Security.Config.EmailValidator;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+    private EmailValidator emailValidator;
     public String register(RegistrationRequest request) {
+        emailValidator.test(request.getEmail());
         return "it works";
     }
 }
