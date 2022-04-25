c7a201a6e87069de9be2306ec113323d28747885
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-25 11:20:30 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Security/Config/SecurityConfig.java
+++ src/main/java/app/logreg/v1/Security/Config/SecurityConfig.java
@@ -1,4 +1,11 @@
 package app.logreg.v1.Security.Config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
 }
