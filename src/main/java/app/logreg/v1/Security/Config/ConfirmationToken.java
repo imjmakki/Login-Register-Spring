a1ad778da498e31717bce36c942611000082442c
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-25 14:12:43 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Security/Config/ConfirmationToken.java
+++ src/main/java/app/logreg/v1/Security/Config/ConfirmationToken.java
@@ -4,13 +4,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class ConfirmationToken {
+
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
+    @Column(nullable = false)
     private String token;
     private LocalDateTime createdAt;
     private LocalDateTime expiredAt;
