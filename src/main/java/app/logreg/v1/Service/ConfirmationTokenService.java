0ccd4dcdf0f77b1df2f81993bd42db9f3197a0af
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-25 19:10:48 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Service/ConfirmationTokenService.java
+++ src/main/java/app/logreg/v1/Service/ConfirmationTokenService.java
@@ -1,7 +1,9 @@
 package app.logreg.v1.Service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ConfirmationTokenService {
 }
