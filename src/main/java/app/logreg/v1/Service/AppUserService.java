f7b5d3160386f6fdf3a8b2246640a2a7793e1e36
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-25 13:38:32 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Service/AppUserService.java
+++ src/main/java/app/logreg/v1/Service/AppUserService.java
@@ -33,8 +33,10 @@ public class AppUserService implements UserDetailsService {
         if (userExists) {
             throw new IllegalStateException("email already taken");
         }
+
         String encodedPassword = bCryptPasswordEncoder
                 .encode(appUser.getPassword());
+        appUser.setPassword(encodedPassword);
         return "";
     }
 }
