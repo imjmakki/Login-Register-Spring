c8b891b976f68b8d73ffc12b439723ee9ff39fb0
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-25 11:25:01 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Security/Config/SecurityConfig.java
+++ src/main/java/app/logreg/v1/Security/Config/SecurityConfig.java
@@ -2,6 +2,7 @@ package app.logreg.v1.Security.Config;
 
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,4 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @AllArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/register/**")
+    }
 }
