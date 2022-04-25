04f67f5aa27e99daefae2c6401e21d3d5c36f4e5
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-25 11:36:05 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Security/PasswordEncoder.java
+++ src/main/java/app/logreg/v1/Security/PasswordEncoder.java
@@ -1,10 +1,12 @@
 package app.logreg.v1.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class PasswordEncoder {
+    @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
