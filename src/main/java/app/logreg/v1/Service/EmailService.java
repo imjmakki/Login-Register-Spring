bfead8235b9054d2576b1a99fcf7b9f6ac707640
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-26 09:52:11 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Service/EmailService.java
+++ src/main/java/app/logreg/v1/Service/EmailService.java
@@ -5,9 +5,12 @@ import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 @AllArgsConstructor
@@ -16,9 +19,16 @@ public class EmailService implements EmailSender {
             .getLogger(EmailService.class);
     private final JavaMailSender mailSender;
     @Override
+    @Async
     public void send(String to, String email) {
         try {
-
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper =
+                    new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email");
+            helper.setFrom("mehmet.anbaki@gmail.com");
         } catch (MessagingException e) {
             LOGGER.error("failed to send email", e);
             throw new IllegalStateException("failed to send email");
