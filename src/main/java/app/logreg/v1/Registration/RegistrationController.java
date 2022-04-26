76cf964bdf4a199b7298f7de80c8a608b72553d3
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-26 09:28:42 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Registration/RegistrationController.java
+++ src/main/java/app/logreg/v1/Registration/RegistrationController.java
@@ -3,20 +3,22 @@ package app.logreg.v1.Registration;
 import app.logreg.v1.Security.Config.RegistrationRequest;
 import app.logreg.v1.Service.RegistrationService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1/register")
 @AllArgsConstructor
 public class RegistrationController {
 
-    private RegistrationService registrationService;
+    private final RegistrationService registrationService;
 
     @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
+    }
+
+    @GetMapping(path = "confirm")
+    public String confirm(@RequestParam("token") String token) {
+        return registrationService.confirmToken(token);
     }
 }
