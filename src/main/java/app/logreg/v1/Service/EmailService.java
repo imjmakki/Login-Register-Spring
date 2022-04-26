5be7284dc675ad2424781d2c66862b7642fd0f91
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-26 09:35:10 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Service/EmailService.java
+++ src/main/java/app/logreg/v1/Service/EmailService.java
@@ -2,12 +2,15 @@ package app.logreg.v1.Service;
 
 import app.logreg.v1.DAO.EmailSender;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class EmailService implements EmailSender {
+    private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
     private final JavaMailSender mailSender;
     @Override
     public void send(String to, String email) {
