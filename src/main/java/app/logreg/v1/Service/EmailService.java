af82d285c54eb1ad39215d3ba6eba87b05f24cab
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-26 09:33:09 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Service/EmailService.java
+++ src/main/java/app/logreg/v1/Service/EmailService.java
@@ -1,10 +1,14 @@
 package app.logreg.v1.Service;
 
 import app.logreg.v1.DAO.EmailSender;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class EmailService implements EmailSender {
+    private final JavaMailSender mailSender;
     @Override
     public void send(String to, String email) {
 
