195e543f6eb0955c37949e528e440ed04eac7de7
Mohamad J. Makki <malanbaki@transforat.com>
2022-04-26 10:22:55 +0300

    Login/Register

--- src/main/java/app/logreg/v1/Service/AppUserService.java
+++ src/main/java/app/logreg/v1/Service/AppUserService.java
@@ -41,17 +41,26 @@ public class AppUserService implements UserDetailsService {
 
         String encodedPassword = bCryptPasswordEncoder
                 .encode(appUser.getPassword());
+
         appUser.setPassword(encodedPassword);
+
         appUserRepository.save(appUser);
 
         String token = UUID.randomUUID().toString();
+
         ConfirmationToken confirmationToken = new ConfirmationToken(
                 token,
                 LocalDateTime.now(),
                 LocalDateTime.now().plusMinutes(15),
                 appUser
         );
-        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+        confirmationTokenService.saveConfirmationToken(
+                confirmationToken);
         return token;
+    }
+
+    public int enableAppUser(String email) {
+        return appUserRepository.enableAppUser(email);
     }
 }
